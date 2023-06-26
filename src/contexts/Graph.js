3ac121ff29b908db34cce2fd15dd4b689e56d97b
Sinh Vu <khacsinh.vu.16091994@gmail.com>
2023-06-26 21:48:35 +0700

    remove other chains

--- src/contexts/Graph.js
+++ src/contexts/Graph.js
@@ -2,52 +2,12 @@ import React, { useState, createContext, useEffect } from 'react'
 //The Graph
 import { ApolloClient, InMemoryCache, useQuery } from '@apollo/client'
 //Utils
-import { reporterQuery, autopayQuery } from '../utils/queries'
+import { reporterQuery } from '../utils/queries'
 import { decodingMiddleware, sortDataByProperty } from '../utils/helpers'
 
 export const GraphContext = createContext()
 
 //ApolloClients
-const clientMainnet = new ApolloClient({
-  uri: 'https://gateway.thegraph.com/api/ad08435a6d6c0933c9e272dbdfa21322/subgraphs/id/4mgMy9x1FC6kzjXSQisntEKJFT2U7r73qXMZy2XZ1t4R',
-  cache: new InMemoryCache(),
-})
-const clientGoerli = new ApolloClient({
-  uri: 'https://api.goldsky.com/api/public/project_clf8nopuy59a93stya1d02ev6/subgraphs/tellor-oracle-goerli/v0.0.1/gn',
-  //'https://api.goldsky.com/api/public/project_clf8nopuy59a93stya1d02ev6/subgraphs/tellor-oracle-goerli/v0.0.1/gn',
-  cache: new InMemoryCache(),
-})
-const clientSepolia = new ApolloClient({
-  uri: 'https://api.studio.thegraph.com/query/33329/tellor-oracle-sepolia/v0.0.2',
-  cache: new InMemoryCache(),
-})
-
-const clientMatic = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/tellor-io/tellorflexoraclematichgraph',
-  cache: new InMemoryCache(),
-})
-const clientMumbai = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/tellor-io/tellorflexoraclemumbaihgraph',
-  cache: new InMemoryCache(),
-})
-const clientArbone = new ApolloClient({
-  uri: //'https://api.zondax.ch/fil/data/v1/mainnet/transactions/address/f1bkgyshmwpji4sltshvtyzf6yb7uraxr2pkwlamq?page=1',
-  'https://api.thegraph.com/subgraphs/name/raynharr/tellor-flex-arbitrummain-graph',
-  cache: new InMemoryCache(),
-})
-const clientArbtest = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/tellor-io/tellor-oracle-arbitrum-goerli',
-  //'https://api.goldsky.com/api/public/project_clf8nopuy59a93stya1d02ev6/subgraphs/tellor-oracle-arbitrumtest/v0.0.1/gn',
-  cache: new InMemoryCache(),
-})
-const clientGnosismain = new ApolloClient({
-  uri: 'https://gateway.thegraph.com/api/ad08435a6d6c0933c9e272dbdfa21322/subgraphs/id/A614VZr6wqD4B8wNwiZTqrV6StP1Kvmp2AgG2EdJF31k',
-  cache: new InMemoryCache(),
-})
-const clientOptmain = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/tellor-io/tellor-oracle-optimism-main',
-  cache: new InMemoryCache(),
-})
 const clientPlsmain = new ApolloClient({
   uri: 'https://graph.pulse.domains/subgraphs/name/graphprotocol/tellor-flex',
   cache: new InMemoryCache(),
@@ -59,86 +19,12 @@ const clientPlstest = new ApolloClient({
 
 const Graph = ({ children }) => {
   //Component State
-  const [graphMainnetData, setGraphMainnetData] = useState({})
-  const [graphGoerliData, setGraphGoerliData] = useState({})
-  const [graphSepoliaData, setGraphSepoliaData] = useState({})
-  const [graphMaticData, setGraphMaticData] = useState({})
-  const [graphMumbaiData, setGraphMumbaiData] = useState({})
-  const [graphArboneData, setGraphArboneData] = useState({})
-  const [graphArbtestData, setGraphArbtestData] = useState({})
-  const [graphGnosismainData, setGraphGnosismainData] = useState({})
-  const [graphOptmainData, setGraphOptmainData] = useState({})
   const [graphPlsmainData, setGraphPlsmainData] = useState({})
   const [graphPlstestData, setGraphPlstestData] = useState({})
   const [allGraphData, setAllGraphData] = useState(null)
   const [decodedData, setDecodedData] = useState(null)
 
   //Graph Querying every 5 seconds
-  //Mainnet
-  const mainnet = useQuery(reporterQuery, {
-    client: clientMainnet,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  const mainPay = useQuery(autopayQuery, {
-    client: clientMainnet,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-
-   //Goerli
-   const goerli = useQuery(reporterQuery, {
-    client: clientGoerli,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  /*const goerliPay = useQuery(autopayQuery, {
-    client: clientGoerli,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })*/
-  //Sepolia
-  const sepolia = useQuery(reporterQuery, {
-    client: clientSepolia,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  //Matic
-  const matic = useQuery(reporterQuery, {
-    client: clientMatic,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  //Mumbai
-  const mumbai = useQuery(reporterQuery, {
-    client: clientMumbai,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  //Arbitrum One (Main)
-  const arbone = useQuery(reporterQuery, {
-    client: clientArbone,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-    //Arbitrum Test (Goerli)
-  const arbtest = useQuery(reporterQuery, {
-    client: clientArbtest,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  //Gnosis Mainnet
-  const gnosismain = useQuery(reporterQuery, {
-    client: clientGnosismain,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  //optimism(mainnet)
-  const optmain = useQuery(reporterQuery, {
-    client: clientOptmain,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
   //pulsechain(mainnet)
   const plsmain = useQuery(reporterQuery, {
     client: clientPlsmain,
@@ -154,124 +40,6 @@ const Graph = ({ children }) => {
 
   //useEffects for listening to reponses
   //from ApolloClient queries
-  //Mainnet
-  useEffect(() => {
-    if (!mainnet) return
-    setGraphMainnetData({
-      data: mainnet.data,
-      loading: mainnet.loading,
-      error: mainnet.error,
-    })
-    return () => {
-      setGraphMainnetData({})
-    }
-  }, [mainnet.data, mainnet.loading, mainnet.error]) //eslint-disable-line
-
-    //Goerli
-    useEffect(() => {
-      if (!goerli) return
-      setGraphGoerliData({
-        data: goerli.data,
-        loading: goerli.loading,
-        error: goerli.error,
-      })
-  
-      return () => {
-        setGraphGoerliData({})
-      }
-    }, [goerli.data, goerli.loading, goerli.error]) //eslint-disable-line
-    //Sepolia
-    useEffect(() => {
-      if (!sepolia) return
-      setGraphSepoliaData({
-        data: sepolia.data,
-        loading: sepolia.loading,
-        error: sepolia.error,
-      })
-  
-      return () => {
-        setGraphSepoliaData({})
-        console.log(setGraphSepoliaData)
-      }
-    }, [sepolia.data, sepolia.loading, sepolia.error]) //eslint-disable-line
-  //Matic
-  useEffect(() => {
-    if (!matic) return
-    setGraphMaticData({
-      data: matic.data,
-      loading: matic.loading,
-      error: matic.error,
-    })
-
-    return () => {
-      setGraphMaticData({})
-    }
-  }, [matic.data, matic.loading, matic.error]) //eslint-disable-line
-  //Mumbai
-  useEffect(() => {
-    if (!mumbai) return
-    setGraphMumbaiData({
-      data: mumbai.data,
-      loading: mumbai.loading,
-      error: mumbai.error,
-    })
-
-    return () => {
-      setGraphMumbaiData({})
-    }
-  }, [mumbai.data, mumbai.loading, mumbai.error]) //eslint-disable-line
-   //Arbitrum One
-   useEffect(() => {
-    if (!arbone) return
-    setGraphArboneData({
-      data: arbone.data,
-      loading: arbone.loading,
-      error: arbone.error,
-    })
-
-    return () => {
-      setGraphArboneData({})
-    }
-  }, [arbone.data, arbone.loading, arbone.error]) //eslint-disable-line  
-     //Arbitrum Test
-     useEffect(() => {
-      if (!arbtest) return
-      setGraphArbtestData({
-        data: arbtest.data,
-        loading: arbtest.loading,
-        error: arbtest.error,
-      })
-  
-      return () => {
-        setGraphArbtestData({})
-      }
-    }, [arbtest.data, arbtest.loading, arbtest.error]) //eslint-disable-line  
-  //Gnosis Main
-  useEffect(() => {
-    if (!gnosismain) return
-    setGraphGnosismainData({
-      data: gnosismain.data,
-      loading: gnosismain.loading,
-      error: gnosismain.error,
-    })
-
-    return () => {
-      setGraphGnosismainData({})
-    }
-  }, [gnosismain.data, gnosismain.loading, gnosismain.error]) //eslint-disable-line 
-  //Optmain
-  useEffect(() => {
-    if (!optmain) return
-    setGraphOptmainData({
-      data: optmain.data,
-      loading: optmain.loading,
-      error: optmain.error,
-    })
-
-    return () => {
-      setGraphOptmainData({})
-    }
-  }, [optmain.data, optmain.loading, optmain.error]) //eslint-disable-line
   //PulseChain(mainnet)
   useEffect(() => {
     if (!plsmain) return
@@ -301,67 +69,9 @@ const Graph = ({ children }) => {
 
   //For conglomerating data
   useEffect(() => {
-    if (
-      !graphMainnetData.data ||
-      !graphGoerliData.data ||
-      !graphSepoliaData.data ||
-      !graphMaticData.data ||
-      !graphMumbaiData.data ||
-      !graphArboneData.data ||
-      !graphArbtestData.data ||
-      !graphGnosismainData.data ||
-      !graphOptmainData.data ||
-      !graphPlsmainData.data ||
-      !graphPlstestData.data
-    )
-      return
+    if (!graphPlsmainData.data || !graphPlstestData.data) return
 
     let eventsArray = []
-   graphMainnetData.data.newReportEntities.forEach((event) => {
-      event.chain = 'Ethereum Mainnet'
-      event.txnLink = `https://etherscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    graphGoerliData.data.newReportEntities.forEach((event) => {
-      event.chain = 'Goerli Testnet'
-      event.txnLink = `https://goerli.etherscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    graphSepoliaData.data.newReportEntities.forEach((event) => {
-      event.chain = 'Sepolia Testnet'
-      event.txnLink = `https://sepolia.etherscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    graphMaticData.data.newReportEntities.forEach((event) => {
-      event.chain = 'Polygon Mainnet'
-      event.txnLink = `https://polygonscan.com/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    graphMumbaiData.data.newReportEntities.forEach((event) => {
-      event.chain = 'Mumbai Testnet'
-      event.txnLink = `https://mumbai.polygonscan.com/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    graphArboneData.data.newReportEntities.forEach((event) => {
-      event.chain = 'Arbitrum Mainnet'
-      event.txnLink = `https://arbiscan.io//tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    graphArbtestData.data.newReportEntities.forEach((event) => {
-      event.chain = 'Arbitrum Goerli'
-      event.txnLink = `https://goerli.arbiscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    graphGnosismainData.data.newReportEntities.forEach((event) => {
-      event.chain = 'Gnosis Mainnet'
-      event.txnLink = `https://gnosisscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    graphOptmainData.data.newReportEntities.forEach((event) => {
-      event.chain = 'Optimism Mainnet'
-      event.txnLink = `https://optimistic.etherscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
     graphPlsmainData.data.newReportEntities.forEach((event) => {
       event.chain = 'PulseChain (Mainnet)'
       event.txnLink = `https://scan.pulsechain.com/tx/${event.txnHash}`
@@ -378,19 +88,7 @@ const Graph = ({ children }) => {
     return () => {
       setAllGraphData(null)
     }
-  }, [
-    graphMainnetData,
-    graphGoerliData,
-    graphSepoliaData,
-    graphMaticData,
-    graphMumbaiData,
-    graphArboneData,
-    graphArbtestData,
-    graphGnosismainData,
-    graphOptmainData,
-    // graphPlsmainData,
-    // graphPlstestData
-  ])
+  }, [graphPlsmainData, graphPlstestData])
 
   useEffect(() => {
     if (!allGraphData) return
@@ -405,7 +103,7 @@ const Graph = ({ children }) => {
     decodedData: decodedData,
   }
 
-   // console.log(graphArboneData)
+  // console.log(graphArboneData)
 
   return (
     <GraphContext.Provider value={GraphContextObj}>
--- src/contexts/GraphAutopay.js
+++ src/contexts/GraphAutopay.js
@@ -3,43 +3,12 @@ import React, { useState, createContext, useEffect } from 'react'
 import { ApolloClient, InMemoryCache, useQuery } from '@apollo/client'
 //Utils
 import { autopayQuery } from '../utils/queries'
-import { decodingAutopayMiddleware, sortDataByProperty  } from '../utils/helpers'
+import { decodingAutopayMiddleware, sortDataByProperty } from '../utils/helpers'
 //Sort
 
 export const GraphAutopayContext = createContext()
 
 //ApolloClients
-
-const clientMumbai = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/tellor-io/tellorautopaymumbaihgraph',
-  cache: new InMemoryCache(),
-})
-
-const clientMainnet = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/raynharr/tellor-autopay-ethereum-graph',
-  cache: new InMemoryCache(),
-})
-
-const clientMatic = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/tellor-io/tellorautopaymatichgraph',
-  cache: new InMemoryCache(),
-})
-
-const clientGoerli = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/raynharr/tellor-autopay-goerli-graph',
-  cache: new InMemoryCache(),
-})
-
-const clientSepolia = new ApolloClient({
-  uri: 'https://api.studio.thegraph.com/query/33329/tellor-autopay-sepolia/v0.0.3',
-  cache: new InMemoryCache(),
-})
-
-const clientOpmain = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/tellor-io/tellor-autopay-optimism-main',
-  cache: new InMemoryCache(),
-})
-
 const clientPlsmain = new ApolloClient({
   uri: 'https://graph.pulse.domains/subgraphs/name/graphprotocol/tellor-autopay',
   cache: new InMemoryCache(),
@@ -52,53 +21,11 @@ const clientPlstest = new ApolloClient({
 
 const GraphAutopay = ({ children }) => {
   //Component State
-  const [autopayMaticData, setAutopayMaticData] = useState({})
-  const [autopayMainnetData, setAutopayMainnetData] = useState({})
-  const [autopayMumbaiData, setAutopayMumbaiData] = useState({})
-  const [autopayGoerliData, setAutopayGoerliData] = useState({})
-  const [autopaySepoliaData, setAutopaySepoliaData] = useState({})
-  const [autopayOpmainData, setAutopayOpmainData] = useState({})
   const [autopayPlsmainData, setAutopayPlsmainData] = useState({})
   const [autopayPlstestData, setAutopayPlstestData] = useState({})
   const [decodedData, setDecodedData] = useState([])
   const [allGraphData, setAllGraphData] = useState(null)
   //Context State
-  //Matic
-  const matic = useQuery(autopayQuery, {
-    client: clientMatic,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  //Eth mainnet
-  const mainnet = useQuery(autopayQuery, {
-    client: clientMainnet,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  //Mumbai
-  const mumbai = useQuery(autopayQuery, {
-    client: clientMumbai,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  //Goerli
-  const goerli = useQuery(autopayQuery, {
-    client: clientGoerli,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  //Sepolia
-  const sepolia = useQuery(autopayQuery, {
-    client: clientSepolia,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
-  //Optimism
-  const opmain = useQuery(autopayQuery, {
-    client: clientOpmain,
-    fetchPolicy: 'network-only',
-    pollInterval: 5000,
-  })
   //Pulsechain(mainnet)
   const plsmain = useQuery(autopayQuery, {
     client: clientPlsmain,
@@ -111,83 +38,9 @@ const GraphAutopay = ({ children }) => {
     fetchPolicy: 'network-only',
     pollInterval: 5000,
   })
-  
+
   //useEffects for listening to reponses
   //from ApolloClient queries
-  //Matic
-  useEffect(() => {
-    if (!matic) return
-    setAutopayMaticData({
-      data: matic.data,
-      loading: matic.loading,
-      error: matic.error,
-    })
-    return () => {
-      console.log(matic.data)
-
-      setAutopayMaticData({})
-    }
-  }, [matic.data, matic.loading, matic.error]) //eslint-disable-line
-  //Eth Mainnet
-  useEffect(() => {
-    if (!mainnet) return
-    setAutopayMainnetData({
-      data: mainnet.data,
-      loading: mainnet.loading,
-      error: mainnet.error,
-    })
-    return () => {
-      setAutopayMainnetData({})
-    }
-  }, [mainnet.data, mainnet.loading, mainnet.error]) //eslint-disable-line*/
-    //Goerli
-    useEffect(() => {
-      if (!goerli) return
-      setAutopayGoerliData({
-        data: goerli.data,
-        loading: goerli.loading,
-        error: goerli.error,
-      })
-      return () => {
-        setAutopayGoerliData({})
-      }
-    }, [goerli.data, goerli.loading, goerli.error]) //eslint-disable-line*/
-    //Sepolia
-    useEffect(() => {
-      if (!sepolia) return
-      setAutopaySepoliaData({
-        data: sepolia.data,
-        loading: sepolia.loading,
-        error: sepolia.error,
-      })
-      return () => {
-        setAutopaySepoliaData({})
-      }
-    }, [sepolia.data, sepolia.loading, sepolia.error]) //eslint-disable-line*/
-  //Mumbai
-  useEffect(() => {
-    if (!mumbai) return
-    setAutopayMumbaiData({
-      data: mumbai.data,
-      loading: mumbai.loading,
-      error: mumbai.error,
-    })
-    return () => {
-      setAutopayMumbaiData({})
-    }
-  }, [mumbai.data, mumbai.loading, mumbai.error]) //eslint-disable-line
-  //Optimism
-  useEffect(() => {
-    if (!opmain) return
-    setAutopayOpmainData({
-      data: opmain.data,
-      loading: opmain.loading,
-      error: opmain.error,
-    })
-    return () => {
-      setAutopayOpmainData({})
-    }
-  }, [opmain.data, opmain.loading, opmain.error]) //eslint-disable-line
   //Pulsechain(mainnet)
   useEffect(() => {
     if (!plsmain) return
@@ -214,80 +67,9 @@ const GraphAutopay = ({ children }) => {
   }, [plstest.data, plstest.loading, plstest.error]) //eslint-disable-line
   //useEffects for decoding autopay events
   useEffect(() => {
-    if (
-      !autopayMaticData.data ||
-      !autopayMainnetData.data ||
-      !autopayGoerliData.data ||
-      !autopaySepoliaData.data ||
-      !autopayMumbaiData.data ||
-      !autopayOpmainData.data ||
-      !autopayPlsmainData.data ||
-      !autopayPlstestData.data 
-    )
-      return
+    if (!autopayPlsmainData.data || !autopayPlstestData.data) return
 
     let eventsArray = []
-
-    autopayMaticData.data.dataFeedEntities.forEach((event) => {
-      event.chain = 'Polygon Mainnet'
-      event.txnLink = `https://polygonscan.com/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    autopayMaticData.data.tipAddedEntities.forEach((event) => {
-      event.chain = 'Polygon Mainnet'
-      event.txnLink = `https://polygonscan.com/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    autopayMumbaiData.data.dataFeedEntities.forEach((event) => {
-      event.chain = 'Mumbai Testnet'
-      event.txnLink = `https://mumbai.polygonscan.com/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    autopayMumbaiData.data.tipAddedEntities.forEach((event) => {
-      event.chain = 'Mumbai Testnet'
-      event.txnLink = `https://mumbai.polygonscan.com/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    autopayMainnetData.data.dataFeedEntities.forEach((event) => {
-      event.chain = 'Ethereum Mainnet'
-      event.txnLink = `https://etherscan.com/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    autopayMainnetData.data.tipAddedEntities.forEach((event) => {
-      event.chain = 'Ethereum Mainnet'
-      event.txnLink = `https://etherscan.com/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    autopayGoerliData.data.dataFeedEntities.forEach((event) => {
-      event.chain = 'Goerli Testnet'
-      event.txnLink = `https://goerli.etherscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    autopayGoerliData.data.tipAddedEntities.forEach((event) => {
-      event.chain = 'Goerli Testnet'
-      event.txnLink = `https://goerli.etherscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    autopaySepoliaData.data.dataFeedEntities.forEach((event) => {
-      event.chain = 'Sepolia Testnet'
-      event.txnLink = `https://sepolia.etherscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    autopaySepoliaData.data.tipAddedEntities.forEach((event) => {
-      event.chain = 'Sepolia Testnet'
-      event.txnLink = `https://sepolia.etherscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    autopayOpmainData.data.dataFeedEntities.forEach((event) => {
-      event.chain = 'Optimism Mainnet'
-      event.txnLink = `https://optimistic.etherscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
-    autopayOpmainData.data.tipAddedEntities.forEach((event) => {
-      event.chain = 'Optimism Mainnet'
-      event.txnLink = `https://optimistic.etherscan.io/tx/${event.txnHash}`
-      eventsArray.push(event)
-    })
     autopayPlsmainData.data.dataFeedEntities.forEach((event) => {
       event.chain = 'PulseChain (Mainnet)'
       event.txnLink = `https://scan.pulsechain.com/tx/${event.txnHash}`
@@ -314,8 +96,7 @@ const GraphAutopay = ({ children }) => {
     return () => {
       setAllGraphData(null)
     }
-  }, [autopayMaticData, autopayMumbaiData, autopayMainnetData, autopayGoerliData, autopaySepoliaData, autopayOpmainData, autopayPlsmainData, autopayPlstestData])
-
+  }, [autopayPlsmainData, autopayPlstestData])
 
   useEffect(() => {
     if (!allGraphData) return
@@ -325,11 +106,9 @@ const GraphAutopay = ({ children }) => {
     }
   }, [allGraphData])
 
-
   const GraphContextObj = {
     decodedData: decodedData,
   }
-    console.log(autopaySepoliaData)
 
   return (
     <GraphAutopayContext.Provider value={GraphContextObj}>
